2c18abc405c718be18969b85facc3ed13a7c4741
Eric Murano <2396940+emurano@users.noreply.github.com>
2022-07-14 07:28:44 +1000

    Add doc block for generateImage function

--- src/functions/image-generation/generate-image.ts
+++ src/functions/image-generation/generate-image.ts
@@ -1,6 +1,14 @@
 import {Colour} from "./index";
 import {numColours, stepSize} from "../dimensions";
 
+/**
+ * Generates an array of hex codes to be used as the colours for an imag
+ *
+ * @param height The height, in pixels, of the image
+ * @param width The width, in pixels, of the image
+ * @return An array of hex code strings to be used in css. The image's pixels are organised
+ * from left to right and wrapping back to the left after `width` pixels
+ */
 export function generateImage(height: number, width: number) : Colour[] {
     const imageData: Colour[] = [];
     for (let i = 0; i < numColours; i++) {

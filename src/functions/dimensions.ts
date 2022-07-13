98ed34dd07cbbb738b9e3f1e2c85fb2d825bae2e
Eric Murano <2396940+emurano@users.noreply.github.com>
2022-07-14 00:55:04 +1000

    Add proper steps in colour generation

--- src/functions/dimensions.ts
+++ src/functions/dimensions.ts
@@ -5,3 +5,8 @@ export function calculateHeightFromWidth(width: number, numPixels: number) : num
 export function calculateWidthFromHeight(height: number, numPixels: number) : number {
     return Math.ceil(numPixels / height);
 }
+
+const numSteps = 32;
+export const componentSize = 256;
+export const stepSize = componentSize / numSteps;
+export const numColours = Math.pow(numSteps, 3);
--- src/functions/image-generation/generate-image.ts
+++ src/functions/image-generation/generate-image.ts
@@ -1,11 +1,12 @@
 import {Colour} from "./index";
+import {numColours, stepSize} from "../dimensions";
 
 export function generateImage(height: number, width: number) : Colour[] {
     const imageData: Colour[] = [];
-    for (let i = 0; i < (height * width); i++) {
-        const hex = i.toString(16).padStart(6, "0");
+    for (let i = 0; i < numColours; i++) {
+        const colourIndex = i * stepSize;
+        const hex = colourIndex.toString(16).padStart(6, "0");
         imageData.push(`#${hex}`);
     }
-
     return imageData;
 }

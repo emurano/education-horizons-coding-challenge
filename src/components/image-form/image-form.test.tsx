9b5c2486f366362304c3d51f1c75e66ba9d22dbb
Eric Murano <2396940+emurano@users.noreply.github.com>
2022-07-13 22:02:14 +1000

    width field tests for non-number entry

--- src/components/image-form/image-form.test.tsx
+++ src/components/image-form/image-form.test.tsx
@@ -2,8 +2,27 @@ import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import ImageForm from "./image-form";
 
-test('displays a field for the width', () => {
-    render(<ImageForm />)
 
-    expect(screen.getByTestId('image-form-width-field')).toBeTruthy();
+describe('image form width field', () => {
+    const getWidthField = () => screen.getByTestId('image-form-width-field') as HTMLInputElement;
+
+    test('is rendered in th ImageForm component', () => {
+        render(<ImageForm />);
+
+        expect(getWidthField()).toBeTruthy();
+    })
+
+    test('keeps the original number value when a non-number is entered', () => {
+        render(<ImageForm />)
+
+        fireEvent.change(getWidthField(), {target: {value: 'A'}});
+        expect(getWidthField().value).toBe('256');
+    });
+
+    test('has zero value when field contents is deleted', () => {
+        render(<ImageForm />)
+
+        fireEvent.change(getWidthField(), {target: {value: ''}});
+        expect(getWidthField().value).toBe('0');
+    });
 })
